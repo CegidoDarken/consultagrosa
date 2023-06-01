f31e3da7816c594799c2e0832de8115976ed642b
Cegido Darken <cegidocarlos@gmail.com>
2023-06-01 00:41:13 -0500

    first commit

--- src/database.js
+++ src/database.js
@@ -1,25 +1,14 @@
 // connecting to db
-const mysql = require('mysql');
+const {createPool} = require('mysql2/promise');
 const env = require('dotenv');
 require("colors");
 env.config();
-let connection = mysql.createConnection({
+let connection = createPool({
     host: process.env.DBHOST,
     user: process.env.DBUSER,
     password: process.env.DBPASSWORD,
     database: process.env.DBNAME,
     port: process.env.DBPORT
-});
-
-connection.connect((err) => {
-    console.log("Connecting to the database...".yellow);
-    if (err) {
-        console.log(`Database: ${connection.state.red}`);
-        console.log(`ERROR: ${err.message}`.red);
-    } else {
-        console.log(`Database: ${connection.state.green}`);
-        console.log(`Connected to ${connection.config.database}`.yellow);
-    };
 });
 
 module.exports = { connection };
